7fc6c5029b633da877044656be5b77b3d2577397
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-07 18:29:31 +0100

    fix(adds plugin_packes to gitignore):

--- plugin_packages/repo-baseline-ruleset-plugins/src/index.js
+++
@@ -1,6 +0,0 @@
-const packageJson = require('../package.json');
-const RuleSet = require('repo-baseline-ruleset');
-
-module.exports = (pluginManager, repoPath) => {
-    return RuleSet(pluginManager, repoPath, packageJson['repo-baseline']);
-}
